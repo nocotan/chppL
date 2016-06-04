cf61881190e836971098a0317fae954e41ec4875
nocotan <noconoco.lib@gmail.com>
2016-06-04 13:30:14 +0900

    chppl_validator

--- chppl/chppl_validator.py
+++ chppl/chppl_validator.py
@@ -2,32 +2,34 @@
 C/C++ package management system
 created by @nocotan
 """
-import urllib2
 from urlparse import urlparse
 
 
 class ChpplValidator:
     def __init__(self):
+        self.__msg_list = []
         self.__URL_IS_NOT_VALID = "URL is not valid."
+        self.__URL_IS_NOT_GITHUB = "URL is not Github."
 
     def __del__(self):
         pass
 
     def isExitURL(self, url):
-        try:
-            f = urllib2.urlopen(url)
-            f.close()
-        except urllib2.HTTPError:
-            return self.__URL_IS_NOT_VALID
+        result = urlparse(url).scheme
+        if str(result) == 'http' or str(result) == 'https':
+            return True
+        else:
+            self.__msg_list.append(self.__URL_IS_NOT_VALID)
+            return False
 
     def isGithubURL(self, url):
         result = urlparse(url).netloc
         if str(result) == 'github.com' or str(result) == 'www.github.com':
             return True
         else:
+            self.__msg_list.append(self.__URL_IS_NOT_GITHUB)
             return False
 
 if __name__ == '__main__':
     c = ChpplValidator()
-    print c.isGithubURL("https://github.com/nocotan")
-    print c.isGithubURL("https://sss.com")
+    print c.isExitURL("htts://githb.com/nocotan")
